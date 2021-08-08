fdd778305059a0d0a3a9913ecfa3191221281a7f
Hetman <gggomelll@gmail.com>
2021-08-08 15:52:26 +0300

    Solution #714

--- src/Solution700.java
+++ src/Solution700.java
@@ -2,40 +2,21 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Solution700 extends Scaner {
-    public static void main(String[] args) throws IOException {
-    ArrayList <Integer> list = new ArrayList<>();
-        System.out.println("Enter 20 int numbers: ");
+    public static void main(String[] args) {
+    ArrayList <String> list = new ArrayList<>();
 
-        for (int i = 0; i <10 ; i++) {
-            int a = Integer.parseInt(sc.nextLine());
-            list.add(a);
+        for (int i = 0; i <5 ; i++) {
+            list.add(sc.nextLine());
         }
-        ArrayList <Integer> three = new ArrayList<>();
-        ArrayList <Integer> two = new ArrayList<>();
-        ArrayList <Integer> another = new ArrayList<>();
+        list.remove(2);
 
-        for (int arr: list) {
-          if (arr%3 == 0)
-              three.add(arr);
+        for (int i = list.size() -1; i >= 0; i--) {
+            System.out.println(list.get(i));
         }
-        for (int arr: list) {
-            if (arr % 2 == 0)
-                two.add(arr);
-        }
-        for (int arr: another) {
-            if ((arr % 3 != 0) && (arr % 2 != 0))
-                another.add(arr);
 
         }
-        System.out.println("Arr three");
-        PrintList(three);
-        System.out.println("Arr two");
-        PrintList(two);
-        System.out.println("Arr another");
-        PrintList(another);
-
     }
-}
+
 
 
 
