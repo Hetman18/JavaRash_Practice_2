94d612eb5a9e31e08ae832316c0e151b690a18a4
Hetman <gggomelll@gmail.com>
2021-08-08 19:04:31 +0300

    Empty 700 Class

--- src/Solution700.java
+++ src/Solution700.java
@@ -4,22 +4,7 @@ import java.util.ArrayList;
 
 public class Solution700 extends Methods {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
 
-        for (int i = 0; i <5 ; i++) {
-            list.add(sc.nextLine());
-        }
-            ArrayList<String> result = doubleValues(list);
-            for (String s: result) {
-                System.out.println(s);
-            }
-
-        }
-        public static ArrayList<String> doubleValues(ArrayList<String> list){
-            for (int i = 0; i <list.size() ; i += 2) {
-                list.add(i + 1, list.get(i));
-            }
-            return list;
         }
     }
 
