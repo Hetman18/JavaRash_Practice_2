e51b2a96f77d4b18d5e2f9f07eaae329a9013e6a
Hetman <gggomelll@gmail.com>
2021-08-08 16:28:51 +0300

    Solution 715

--- src/Solution700.java
+++ src/Solution700.java
@@ -1,21 +1,24 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Solution700 extends Scaner {
     public static void main(String[] args) {
-    ArrayList <String> list = new ArrayList<>();
-
-        for (int i = 0; i <5 ; i++) {
-            list.add(sc.nextLine());
+        ArrayList <String> arr = new ArrayList<>();
+        arr.add("мама");
+        arr.add("мыла");
+        arr.add("раму");
+        String word = "именно";
+        arr.add(1, word);
+        arr.add(3, word);
+        arr.add(5, word);
+        for (String arrayList : arr) {
+            System.out.println(arrayList);
         }
-        list.remove(2);
-
-        for (int i = list.size() -1; i >= 0; i--) {
-            System.out.println(list.get(i));
-        }
 
         }
-    }
+        }
+
 
 
 
